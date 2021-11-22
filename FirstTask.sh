9ef4abd68eaf4d19831f3d543e140db0403a9a79
Ekashnik <andrewpavlusyk@gmail.com>
2021-11-22 19:32:14 +0200

    Add files via upload

---
+++ FirstTask.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+username=$1
+folder=$2
+
+if [[ ! $(id $username) ]]; 
+    if [ ! -d "$folder" ]; 
+        chown -R $username:$username $folder
+        echo "complete"
+        exit
+    then
+    echo "Directory does not exist"
+    exit
+    fi
+then
+  echo "User does not exist"
+  exit
+fi
